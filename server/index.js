8b2b529b97606ee8ee488dc14b9e42e6bd742435
Alejandro SDS <33510391+acanequeisban@users.noreply.github.com>
2025-06-17 07:09:28 +0200

    update gen certs and domain

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const HTTPS_PORT = process.env.HTTPS_PORT || 3443;
 
 // Configuración del servidor
 const rpName = 'SPC Demo';
-const rpID = 'demo.savagesoftware.dev';
+const rpID = 'gt008990n64426p.sgt.sgtech.corp';
 const origin = `https://${rpID}`;
 
 // Rutas de archivos
